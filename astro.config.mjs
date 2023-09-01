499cb1dfe133c802c86551f7d0d5c09e833b945b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-01 16:57:03 +0200

    deploy : fixing yaml .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
+// import { defineConfig } from 'astro/config'
+
+// export default defineConfig({
+//   site: 'https://astronaut.github.io',
+//   base: '/ipa',
+//   build: {
+//     inlineStylesheets: 'auto'
+//   }
+// })
+
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://nuoxoxo.github.io',
   base: '/ipa',
-  build: {
-    inlineStylesheets: 'auto'
-  }
 })
